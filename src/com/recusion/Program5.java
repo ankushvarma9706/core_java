9fe337c0535d787aa5659b923d4972c2ad18cab1
ankushvarma9706 <ankush.varma9706@gmail.com>
2025-11-10 19:31:51 +0530

    10/11/2025

---
+++ src/com/recusion/Program5.java
@@ -0,0 +1,16 @@
+package com.stringexamples.com.recusion;
+
+public class Program5 {
+    static void m1(int num)
+    {
+        if(num>0){
+            m1(num-1);
+            m1(num-1);
+            System.out.print(num);
+        }
+    }
+    public static void main(String[] args)
+    {
+        Program1.m1(3);
+    }
+}
